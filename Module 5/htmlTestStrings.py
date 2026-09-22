a1f27a09ac52c76ff14c65f70b17aebb157c64cf
michael mccrea <michaelmcc005@gmail.com>
2026-09-22 12:34:10 -0600

    created html checker file

---
+++ Module 5/htmlTestStrings.py
@@ -0,0 +1,54 @@
+from pythonds3.basic import Stack
+
+def checkTags(tags):
+   s = Stack()
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+# test_balanced = """<html>
+#    <head>
+#       <title>
+#          Example
+#       </title>
+#    </head>
+ 
+#    <body>
+#       <h1>Hello, world</h1>
+#    </body>
+# </html>
+# """
+   
+# test_mismatch ="""<html>
+#    <body>
+#       <h1>Hello, world</h2>
+#    </body>
+# </html>
+# """
+
+# test_missingClose =  """<html>
+#    <head>
+#       <title>Example</title>
+#    </head>
+#    <body>
+#       <h1>Hello, world</h1>
+# </html>
+# """
+
+# test_extraClose = """<html>
+#    <body>
+#    </body>
+# </html>
+# </div>
+# """
+
